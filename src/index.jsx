a0de365f5eaac260165560a124ff926b38d2bd32
Rabia Kahya <rabiakahya22@gmail.com>
2022-12-20 21:32:10 +0300

    Done with the DataBase

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import './index.css';
 
@@ -9,9 +7,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+
       <App />
-    </Provider>
-  </React.StrictMode>
+
 );
