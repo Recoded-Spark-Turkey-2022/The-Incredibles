2f8c615fd414ce2eb6b800c434c617f78c9d1490
Ahmad Al Hariri <100470190+ahmadmhd357@users.noreply.github.com>
2022-12-23 00:52:20 +0300

    creating the stories component and little fix in firbase.js (eslint error)

--- envConfig.js
+++ envConfig.js
@@ -1,6 +1,6 @@
 export const apiKey = process.env.REACT_API_KEY;
 export const authDomain = process.env.REACT_AUTH_DOMAIN;
-export const projectId = process.env.REACT_PROJECT_ID ;
+export const projectId = process.env.REACT_PROJECT_ID;
 export const storageBucket = process.env.REACT_STORAGE_BUCKET;
 export const messagingSenderId = process.env.REACT_MESSAGING_SENDER_ID;
 export const appId = process.env.REACT_APP_ID;
