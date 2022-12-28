098f75f2d640e272264f42110f041eb57eb35e14
Miray Sonmez <sonmezmiray@gmail.com>
2022-12-28 22:22:51 +0300

    Style: Responsiveness

--- src/pages/About/Team.jsx
+++ src/pages/About/Team.jsx
@@ -50,7 +50,7 @@ const MEMBERS=[
         id:7,
     },
     {
-        name: "Osman",
+        name: "Can",
         occupation: "Jr Front-end Web Developer",
         country: "Lebanon",
         id:8,
@@ -62,12 +62,12 @@ const COUNTRIES=["View All", "USA", "Turkey", "Lebanon"]
 
 function Team() {
     const [category, setCategory]=useState("View All");
-
+    const [show, setShow] = useState(true);
 
     const renderBtn = COUNTRIES.map(country=>{
-        const className = country === category ? "rounded-full inline-block px-3 py-2 border-solid border-2 border-cyan-700 bg-cyan-700 text-white font-bold text-xl" : "rounded-full px-3 py-2 mx-1 text-cyan-700 text-xl font-bold";
+        const className = country === category ? "rounded-full inline-block w-32 py-1 border-solid border-2 lg:border-cyan-700 lg:bg-cyan-700 text-white font-bold text-xl sm: border-orange-300 sm: bg-orange-300" : "rounded-full w-32 py-1 mx-1 text-xl font-bold lg:text-cyan-700 lg:bg-transparent sm: border-gray-400 sm: bg-gray-400 sm: text-gray-50";
         return (
-            <div key={country} className="p-3">
+            <div key={country} className="p-3 flex justify-center">
           <button 
             className={className}
             onClick={()=> setCategory(country)}
@@ -81,19 +81,29 @@ function Team() {
       const memberCard = memberFilter.map((member)=>{
         return(
             <div key={member.id} className="flex flex-col justify-start p-10">
-                <h3 className="text-xl p-2 font-bold text-cyan-700">{member.name}</h3>
-                <p className="text-xl p-2 text-cyan-700">{member.occupation}</p>
+                <h3 className="lg:text-xl lg:p-2 font-bold text-cyan-700 sm:text-sm">{member.name}</h3>
+                <p className="lg:text-xl lg:p-2 text-cyan-700 sm: text-xs">{member.occupation}</p>
             </div>
         )
       })
 
+      
+
     return (
-        <section name="team" className="bg-emerald-50 min-h-screen">
-            <h2 className="text-center text-5xl pt-20 pb-20 font-bold text-cyan-700">Our Team</h2>
-            <div name="country-filter" className="flex justify-center pt-10">
+        <section name="team" className="bg-emerald-50 min-h-screen min-w-full">
+            <h2 className="text-center text-5xl font-bold text-cyan-700 lg:pb-10 lg:pt-20 sm: pt-5 pb-2">Our Team</h2>
+            <div className="text-center pt-5">
+                <button onClick={() => setShow((s) => !s)} className="rounded-full inline-block w-32 py-1 border-solid border-2 text-white font-bold text-xl border-orange-300 bg-orange-300 lg:hidden">
+                    {!show ? "Show All" : "Hide"}
+                </button>
+                <div className="grid grid-cols-2 px-10 lg:hidden">
+                {show ? memberCard : null}
+                </div>
+            </div>
+            <div name="country-filter" className="flex justify-center lg:pt-5 lg:flex-row sm: flex-col text-center pt-3">
                 {renderBtn}
             </div>
-            <div name="team" className="flex flex-wrap justify-center p-20">
+            <div name="team" className="lg:flex flex-wrap justify-center lg:p-10 sm: hidden">
                 {memberCard}
             </div>
         </section>
