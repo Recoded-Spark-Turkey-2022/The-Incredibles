3f406def3000915bd60cb462f37493fe2d0fe644
Ahmad Al Hariri <100470190+ahmadmhd357@users.noreply.github.com>
2022-12-30 17:58:22 +0300

    our story section fix

--- src/pages/About/Story.jsx
+++ src/pages/About/Story.jsx
@@ -3,26 +3,26 @@ import AboutPHero from '../../assets/pics/aboutpage/aboutp-hero.svg';
 
 function Story() {
   return (
-    <section name="story" className="flex flex-col h-screen">
+    <section name="story" className="">
       <div
         name="hero-image"
-        className="bg-gradient-to-t from-gray-300 to-transparent max-w-screen h-1/2"
+        className="bg-gradient-to-t from-gray-300 flex justify-center items-center to-transparent w-screen h-1/2  "
       >
-        <img src={AboutPHero} className="w-screen h-full" />
+        <img src={AboutPHero} className="w-full h-full  " />
       </div>
-      <div name="story" className="max-w-screen h-1/2">
-        <h2 className="text-center text-6xl pt-20 pb-10 font-bold text-cyan-700">
+      <div name="story" className="w-screen h-1/2  mt-16  flex flex-col text-center items-center justify-center">
+        <h2 className="text-center  mb-10  text-6xl  font-bold text-cyan-700">
           Our Story
         </h2>
-        <div className="px-20 mx-20 pb-20">
-          <p className="text-center px-20 mx-20 font text-xl max-w-screen">
+        
+          <p className=" w-3/4 font-medium text-lg max-lg:w-full px-10 pb-20">
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal
             distribution of letters, as opposed to using `Content here, content
             here`, making it look like readable English.
           </p>
-        </div>
+        
       </div>
     </section>
   );
