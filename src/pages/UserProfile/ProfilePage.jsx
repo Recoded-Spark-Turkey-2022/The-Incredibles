8ae3b2a8c7e954215f6e47d48d74d44d7cf758cf
Miray Sonmez <sonmezmiray@gmail.com>
2023-01-02 21:52:29 +0300

    Refactor: put together components in profile page and add UserNavbar details #43

--- src/pages/UserProfile/ProfilePage.jsx
+++ src/pages/UserProfile/ProfilePage.jsx
@@ -6,14 +6,16 @@ import MyAccount from "./MyAccount";
 import WriteBlog from "./WriteBlog";
 import UserDetails from "./UserDetails";
 
-//Here will be used as a container for user datails
+//Here will be used as a container for user datails. 
+// These routes should be connected with login page, when it pages come, they will be modified
+//Profile Page is not connected to anywhere right now we need to ask about that first
 
 function ProfilePage() {
     return (
         <div name="profilepage">
           <UserNavbar />
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/myaccount" element={<MyAccount />} />
             <Route path="/write" element={<WriteBlog />} />
             <Route path="/userdetails" element={<UserDetails />} />
--- src/pages/UserProfile/UserNavbar.jsx
+++ src/pages/UserProfile/UserNavbar.jsx
@@ -10,7 +10,56 @@ import UserProfile from '../assets/pics/userProfil.svg';
 //will be connected to the main structure via Home button
 //when user sign out will be redirected to Home
 function UserNavbar() {
-    return <div></div>
+    const links = [
+        { name: 'Home', link: '/' },
+        { name: 'WriteBlog', link: '/write' },
+        { name: 'MyAccount', link: '/myaccount' },
+        { name: 'SignOut', link: '/' },
+      ];
+
+      const [open, setOpen] = useState(false);
+
+      const linksToDisplay = links.map((link) => (
+        <Link
+          className={
+            open
+              ? 'text-cyan-600 font-bold text-xl p-2 hover:underline decoration-solid'
+              : 'lg:p-7 md:p-4 sm:p-2 text-gray-500 font-medium hover:text-blue-500 duration-500 '
+          }
+          key={link.name}
+          to={link.link}
+        >
+          {link.name}
+        </Link>
+      ));
+
+    return (
+        <div>
+          <nav className="lg:my-10 lg:mx-24  md:my-5 md:mx-10  md:flex justify-between hidden">
+            <div className="flex">
+              <img src={Logo} alt="logo" />
+              <h1 className="ml-3 font-bold text-cyan-600 text-xl">Refubook</h1>
+            </div>
+            <div>{linksToDisplay}</div>
+          </nav>
+          <nav
+            name="sid-bar"
+            className={
+              open
+                ? 'md:hidden rounded-r-3xl border-gray-100 w-9/12 z-50 border-2 h-screen absolute bg-white'
+                : 'md:hidden'
+            }
+          >
+            <button type="button" onClick={() => setOpen(!open)}>
+              <img className="m-7" src={open ? BackAroww : Menu} />
+            </button>
+            {open ? <img className="w-1/3 m-auto" src={UserProfile} /> : null}
+            {open ? (
+              <ul className="flex flex-col  mx-10 mt-24">{linksToDisplay}</ul>
+            ) : null}
+          </nav>
+        </div>
+      );
 }
 
 export default UserNavbar;
