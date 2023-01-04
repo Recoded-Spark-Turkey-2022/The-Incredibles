b55373afbf4c203ddc5ee1eabf5cd30af71ce793
batoul <batoul.tenbakji@gmail.com>
2023-01-04 20:39:36 +0000

    write blog

--- src/pages/UserProfile/WriteBlog.jsx
+++ src/pages/UserProfile/WriteBlog.jsx
@@ -1,13 +1,114 @@
-function WriteBlog() {
-  // will be used for user to create a new blog and will show existing blogs of user
-  // alongside with a "create article" button
+import React, { useState } from 'react';
 
-  // existing blogs of logged in user will be showed with a carousel the bottom part of this page (blogs file might be used)
-  //which will have options (onclick on the blog card)
-  //optionscould be;
-  // changing the blog details with a save button
-  // deleting the existing blogs with a delete button
-  return <div></div>;
+function WriteBlog() {
+  const [title, setTitle] = useState('');
+  const [subTitle, setsubTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [media, setMedia] = useState(null);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handlesubTitleChange = (event) => {
+    setsubTitle(event.target.value);
+  };
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleMediaChange = (event) => {
+    setMedia(event.target.files[0]);
+    console.log(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // submit the form to firebase
+  };
+
+  return (
+    <div>
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-9/12 m-auto max-sm:text-center"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="block text-gray-700 text-3xl font-bold mb-10 text-center">
+          Write new blog
+        </h1>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
+            Title:
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="subTitle"
+          >
+            Sub Title:
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="subTitle"
+            value={subTitle}
+            onChange={handlesubTitleChange}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="content"
+          >
+            Content:
+          </label>
+          <textarea
+            rows={8}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            name="content"
+            value={content}
+            onChange={handleContentChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="media"
+          >
+            Media (image or video):
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="file"
+            name="media"
+            onChange={handleMediaChange}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="max-sm:m-auto h-10 px-10 py-2.5 text-center bg-cyan-600 text-white font-medium text-l leading-tight 
+          rounded-full shadow-md
+          ease-in duration-300 hover:bg-purple-700 hover:shadow-lg  hover:scale-110"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default WriteBlog;
