e5c7ee746d8dd3188764d61fdbe58e0dddebf82a
Miray Sonmez <sonmezmiray@gmail.com>
2023-01-05 15:30:34 +0300

    Style: add for MyAccount and Card responsive

--- src/pages/UserProfile/MyAccount.jsx
+++ src/pages/UserProfile/MyAccount.jsx
@@ -3,10 +3,108 @@ import Slider from 'react-slick';
 import '../../slick.css';
 import '../../slick-theme.css';
 import UserPhoto from "../../assets/pics/profilepage/myaccount-user.svg";
+import BG from "../../assets/pics/profilepage/profilebg.svg";
+import PhoneBG from "../../assets/pics/profilepage/phonebg.svg";
 import { useNavigate } from 'react-router-dom';
 
+  //temporary data
+  const blogs = [
+    {
+      title: 'title',
+      text: 'text',
+      author: 'author',
+      date: 'date',
+      id: 0,
+    },
+    {
+      title: 'title',
+      text: 'text',
+      author: 'author',
+      date: 'date',
+      id: 1,
+    },
+    {
+      title: 'title',
+      text: 'text',
+      author: 'author',
+      date: 'date',
+      id: 2,
+    },
+    {
+      title: 'title',
+      text: 'text',
+      author: 'author',
+      date: 'date',
+      id: 3,
+    },
+    {
+      title: 'title',
+      text: 'text',
+      author: 'author',
+      date: 'date',
+      id: 4,
+    },
+    {
+      title: 'title',
+      text: 'text',
+      author: 'author',
+      date: 'date',
+      id: 5,
+    },
+  ];
+
+
 function MyAccount() {
-  return <div></div>
+  const navigate = useNavigate();
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+        },
+      },
+    ],
+  };
+  
+  return (
+    <section name="myaccount" className="bg-sky-300 lg:p-20 max-lg:p-5">
+      <div className="bg-white rounded-3xl">
+        <div className="lg:p-28 lg:pb-10 max-lg:p-3">
+          <div className="flex flex-col items-center lg:pb-20 max-lg:p-10" onClick={() => navigate('/myaccount/myaccountdetails')}>
+            {/* this part navigates user to MyAccountDetails form which does not have root yet  */}
+            <img className="lg:w-1/5 m-auto" src={UserPhoto} />
+            <h2 className="p-5 font-bold text-lg">User Name</h2>
+          </div>
+          <div>
+            <div className="p-5 max-lg:pr-4">
+              <Slider {...settings}>
+                {blogs.map((blog) => <MyAccountCard key={blog.id} /> )}
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default MyAccount;
+
--- src/pages/UserProfile/MyAccountCard.jsx
+++ src/pages/UserProfile/MyAccountCard.jsx
@@ -3,7 +3,25 @@ import User from '../../assets/pics/profilepage/profilepic.svg';
 import { useNavigate } from 'react-router-dom';
 
 function MyAccountCard() {
-  return <div></div>
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => navigate('/write')}  //this part navigates user to WriteBlog, when it has route, this might be modified
+      className="group h-1/4 border mx-6 max-lg:mx-4 mb-6 rounded-lg shadow-[0_5px_5px_-1px_rgba(0,0,0,0.3)] hover:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)] focus:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)]"
+    >
+      <div className="w-full h-full flex flex-col justify-start p-2 mx-2 flex-wrap">
+        <h1 className="font-extrabold text-lg p-1">Your Blog Title</h1>
+        <p className="font-medium text-lg overflow-hidden transition-all duration-900 h-6 pb-2 group-hover:h-fit group-hover:overflow-visible">
+          Start to write your first blog here!
+        </p>
+        <br />
+        <div className="flex items-center p-2">
+          <img src={User} alt="author" className="w-10" />
+          <h1 className="ml-4 text-cyan-600 font-medium">User Name</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyAccountCard;
