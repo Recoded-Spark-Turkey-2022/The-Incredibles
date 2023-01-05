a09f452addb1f6ec90ca1ee1c92fcd885b2d441e
Miray Sonmez <sonmezmiray@gmail.com>
2023-01-05 18:21:35 +0300

    Style: add responsiveness to MyAccountDetails

--- src/pages/UserProfile/MyAccountDetails.jsx
+++ src/pages/UserProfile/MyAccountDetails.jsx
@@ -9,53 +9,110 @@ function MyAccountDetails() {
   const [photo, setPhoto] = useState("");
 
   function handleSubmit(event) {
-    event.preventDefault()
-    console.log(event.target.username.value)  
+    event.preventDefault()  
   }
 
   return (
-    <section name="myaccountdetails" className="bg-sky-300 lg:p-20 max-lg:p-5">
+    <section name="myaccountdetails" className="bg-sky-300 lg:p-10 max-lg:p-5">
       <div className="bg-white rounded-3xl">
-        <div className="p-28 pb-10 max-lg:p-10">          
-          <form onSubmit={handleSubmit}>
+        <div className="max-lg:p-10">          
+          <form onSubmit={handleSubmit} className="lg:p-40 lg:m-40">
             
-            <label htmlFor="userphoto"></label>
+            <label 
+              className="hidden block text-sky-500 text-sm font-bold mb-2"
+              htmlFor="userphoto"
+            >
+              Upload Photo
+            </label>
             <input
-              type="text"
+              type="file"
               name="userphoto"
+              className='hidden shadow appearance-none border rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
               value={photo}
-              onChange={(e) => setPhoto(e.target.value)}
+              onChange={(e) => setPhoto(e.target.files[0])}
             />
-            <div className="flex flex-col items-center pb-20">
-              <img className="lg:w-1/5 m-auto" src={ChangePhoto} />
+            <div className="flex flex-col items-center lg:pb-20 max-lg:p-10">
+              <img className="lg:w-1/5 m-auto" src={ChangePhoto || photo} alt="avatar-preview"/>
               <h2 className="p-5 font-bold text-lg">User Name</h2>
             </div>
 
-            <label className="block text-sky-500 text-sm font-bold mb-2" htmlFor="username">Name:</label>
-            <input type="text" name="username" className="" value={username} onChange={e => setUsername(e.target.value)} />
-            
-            <label className="block text-sky-500 text-sm font-bold mb-2" htmlFor="usersurname">Surname:</label>
-            <input type="text" name="usersurname" className="" value={usersurname} onChange={e => setUsersurname(e.target.value)} />
+            <div name="usernameholder" className='flex lg:flex-row max-lg:flex-col justify-between w-full'>
+              <div name="username" className='flex flex-col'>
+                <label 
+                  className="block text-sky-500 text-sm font-bold mb-2" 
+                  htmlFor="username"
+                >
+                  Name:
+                </label>
+                <input 
+                  type="text" 
+                  name="username" 
+                  className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                  value={username} 
+                  onChange={e => setUsername(e.target.value)} 
+                />
+              </div>
+              <div name="usersurname" className='flex flex-col'>
+                <label 
+                  className="block text-sky-500 text-sm font-bold mb-2" 
+                  htmlFor="usersurname"
+                >
+                  Surname:
+                </label>
+                <input 
+                  type="text" 
+                  name="usersurname" 
+                  className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                  value={usersurname} 
+                  onChange={e => setUsersurname(e.target.value)} 
+                />
+              </div>
+            </div>
 
             <label
-            className="block text-sky-500 text-sm font-bold mb-2"
-            htmlFor="biography"
+              className="block text-sky-500 text-sm font-bold mb-2"
+              htmlFor="biography"
             >
-            Biography:
+              Biography:
             </label>
             <textarea
               rows={1}
-              className=""
+              className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               name="biography"
               value={biography}
               onChange={e => setBiography(e.target.value)}
             />
             
-            <label className="block text-sky-500 text-sm font-bold mb-2" htmlFor="location">Location:</label>
-            <input type="text" name="location" className="" value={location} onChange={e => setLocation(e.target.value)} />
+            <label 
+              className="block text-sky-500 text-sm font-bold mb-2" 
+              htmlFor="location"
+            >
+              Location:
+            </label>
+            <input 
+              type="text" 
+              name="location" 
+              className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+              value={location} 
+              onChange={e => setLocation(e.target.value)} 
+            />
 
-            <input type="submit" value="Save" className="block text-sky-500 text-sm font-bold mb-2"/>
-            <button className='block text-sky-500 text-sm font-bold mb-2' value="Cancel">Cancel</button>
+            <div name="formbuttons" className='flex flex-row justify-center p-2'>
+              <input 
+                type="submit" 
+                value="Save" 
+                className="px-10 py-2.5 mr-5 bg-cyan-600 text-white font-medium text-l leading-tight
+                rounded-full shadow-md
+                ease-in duration-300 hover:bg-purple-700 hover:shadow-lg hover:scale-110"
+              />
+              <button 
+                className="px-10 py-2.5 ml-5 max-lg:bg-cyan-600 max-lg:text-white lg:bg-white lg:text-cyan-600 lg:border-cyan-600 lg:border-2 font-medium text-l leading-tight
+                rounded-full shadow-md
+                ease-in duration-300 hover:bg-purple-700 hover:shadow-lg hover:scale-110" 
+              >
+                Cancel
+              </button>
+            </div>
           </form>
           <div>
           </div>
