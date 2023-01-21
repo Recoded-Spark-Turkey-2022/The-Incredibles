bd9ba5acb031e437e4995cddaeab5a1c9fe6f166
Miray <sonmezmiray@gmail.com>
2023-01-21 22:35:02 +0300

    Doc: add MyAccountCard back, makes more sense

--- src/pages/UserProfile/MyAccount.jsx
+++ src/pages/UserProfile/MyAccount.jsx
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../../features/users/usersSlice';
 import BlogCard from '../Blogs/BlogCard';
 import User from '../../assets/pics/profilepage/profilepic.svg';
+import MyAccountCard from './MyAccountCard';
 
 function MyAccount() {
   const { blogs } = useSelector((state) => state.blogs);
@@ -64,32 +65,7 @@ function MyAccount() {
                       return <BlogCard key={i} blog={blog} />;
                     })
                 ) : (
-                  <div
-                    onClick={() => navigate('/myaccount/write')}
-                    className="group h-1/4 border mx-6 max-lg:mx-4 mb-6 rounded-lg shadow-[0_5px_5px_-1px_rgba(0,0,0,0.3)] hover:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)] focus:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)]"
-                  >
-                    <div className="transition-all duration-500 w-full h-1/4 bg-gray-200 border py-24 overflow-hidden group-hover:py-16"></div>
-                    <div className="w-full h-full flex flex-col justify-start p-2 mx-2 flex-wrap">
-                      <h1 className="font-extrabold text-lg p-1">
-                        Your Blog Title
-                      </h1>
-                      <p className="font-medium text-lg overflow-hidden transition-all duration-900 h-6 pb-2 group-hover:h-fit group-hover:overflow-visible">
-                        Start to write your first blog here!
-                      </p>
-                      <br />
-                      <div className="flex items-center p-2">
-                        <img
-                          src={user.photoURL ? user.photoURL : User}
-                          alt="author"
-                          className="w-10 rounded-full"
-                        />
-                        <h1 className="ml-4 text-cyan-600 font-medium">
-                          {user.username ? user.username : 'Name'}{' '}
-                          {user.usersurname ? user.usersurname : 'Surname'}
-                        </h1>
-                      </div>
-                    </div>
-                  </div>
+                  <MyAccountCard />
                 )}
               </Slider>
             </div>
