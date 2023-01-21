28348bf8db30827cb72e7ed32558b1486b34dd76
Miray <sonmezmiray@gmail.com>
2023-01-21 17:08:50 +0300

    Doc: delete MyAccountCard and put single empty card in ternary

--- src/pages/UserProfile/MyAccount.jsx
+++ src/pages/UserProfile/MyAccount.jsx
@@ -1,4 +1,3 @@
-import MyAccountCard from './MyAccountCard';
 import Slider from 'react-slick';
 import '../../slick.css';
 import '../../slick-theme.css';
@@ -7,16 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../features/users/usersSlice';
 import BlogCard from '../Blogs/BlogCard';
-
-const emptyCard = [
-  {
-    title: 'title',
-    content: 'content',
-    author: 'author',
-    date: 'date',
-    id: 0,
-  },
-];
+import User from '../../assets/pics/profilepage/profilepic.svg';
 
 function MyAccount() {
   const { blogs } = useSelector((state) => state.blogs);
@@ -67,15 +57,35 @@ function MyAccount() {
           <div>
             <div className="pb-5 max-lg:pr-4">
               <Slider {...settings}>
-                {blogs
-                  ? blogs
-                      .filter((blog) => blog.data.author.authorId === user.id)
-                      .map((blog, i) => {
-                        return <BlogCard key={i} blog={blog} />;
-                      })
-                  : emptyCard.map((holder) => (
-                      <MyAccountCard key={holder.id} />
-                    ))}
+                {blogs ? (
+                  blogs
+                    .filter((blog) => blog.data.author.authorId === user.id)
+                    .map((blog, i) => {
+                      return <BlogCard key={i} blog={blog} />;
+                    })
+                ) : (
+                  <div
+                    onClick={() => navigate('/myaccount/write')}
+                    className="group h-1/4 border mx-6 max-lg:mx-4 mb-6 rounded-lg shadow-[0_5px_5px_-1px_rgba(0,0,0,0.3)] hover:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)] focus:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)]"
+                  >
+                    <div className="transition-all duration-500 w-full h-1/4 bg-gray-200 border py-24 overflow-hidden group-hover:py-16"></div>
+                    <div className="w-full h-full flex flex-col justify-start p-2 mx-2 flex-wrap">
+                      <h1 className="font-extrabold text-lg p-1">
+                        Your Blog Title
+                      </h1>
+                      <p className="font-medium text-lg overflow-hidden transition-all duration-900 h-6 pb-2 group-hover:h-fit group-hover:overflow-visible">
+                        Start to write your first blog here!
+                      </p>
+                      <br />
+                      <div className="flex items-center p-2">
+                        <img src={User} alt="author" className="w-10" />
+                        <h1 className="ml-4 text-cyan-600 font-medium">
+                          User Name
+                        </h1>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </Slider>
             </div>
           </div>
