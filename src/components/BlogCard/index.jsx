072d4b2965edf65ff0acdf66642f13fb31fea9d2
Miray <sonmezmiray@gmail.com>
2023-01-23 17:55:22 +0300

    Refactor: add popover for authordetails in BlogCard

--- src/components/BlogCard/index.jsx
+++ src/components/BlogCard/index.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { parseISO, formatDistanceToNow } from 'date-fns';
+import {
+  Popover,
+  PopoverHandler,
+  PopoverContent,
+} from '@material-tailwind/react';
 
 function BlogCard({ blog }) {
   const { blogs } = useSelector((state) => state.blogs);
@@ -18,34 +23,84 @@ function BlogCard({ blog }) {
   const navigate = useNavigate();
   return (
     <div
-      onClick={handleClick}
       className="group h-1/4 border mx-6 max-lg:mx-4 mb-6 rounded-lg shadow-[0_5px_5px_-1px_rgba(0,0,0,0.3)] hover:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)] focus:shadow-[5px_5px_5px_-1px_rgba(0,0,0,0.3)]"
     >
       <div className="transition-all duration-500 w-full bg-gray-200 border overflow-hidden group-hover:py-1">
         <img
+          onClick={handleClick}
           className="m-auto h-60 w-60"
           src={blog.data.mediaURL ? blog.data.mediaURL : null}
           alt="blog-photo-preview"
         />
       </div>
-
       <div className="w-full h-full flex flex-col justify-start p-2 mx-2 flex-wrap">
+      <div onClick={handleClick}>
         <h1 className="font-bold">{blog.data.title}</h1>
         <p className="font-medium overflow-hidden transition-all duration-900 h-6 pb-2 group-hover:h-fit group-hover:overflow-visible">
           {blog.data.subTitle}
         </p>
         <span>{timeAgo}</span>
-        <span>{thisBlog && thisBlog.data.likedUsers.length} likes</span>
-        <div className="flex items-center ">
-          <img
+        <p>{thisBlog && thisBlog.data.likedUsers.length} likes</p>
+        </div>
+        <div className="flex items-center">
+        <Popover placement="right">
+              <PopoverHandler className="relative">
+              <div>
+              <img
             src={thisBlog && thisBlog.data.author.authorPhoto}
             alt="author"
-            className="w-10 rounded-full"
+            className="w-10 h-10 rounded-full"
           />
-          <h1 className="ml-4 text-cyan-600 font-medium">
+          </div>
+              </PopoverHandler>
+              <PopoverContent className="absolute">
+              <div className='p-5'>
+              <img
+            src={thisBlog && thisBlog.data.author.authorPhoto}
+            alt="author"
+            className="w-10 h-10 rounded-full"
+          />
+          <br />
+          <h2 className="ml-4 text-cyan-600 font-medium text-center">{thisBlog && thisBlog.data.author.authorName}</h2>
+          <br />
+          <h2 className="ml-4 text-cyan-600 font-medium">Biography</h2>
+          <p>biography will be here</p>
+          <br />
+          <h2 className="ml-4 text-cyan-600 font-medium">Location</h2>
+          <p>Location here</p>
+          </div>
+              </PopoverContent>
+            </Popover>
+            <Popover placement="right">
+              <PopoverHandler className="relative">
+              <div>
+              <h1 className="ml-4 text-cyan-600 font-medium">
             {thisBlog && thisBlog.data.author.authorName}
           </h1>
-        </div>
+          </div>
+              </PopoverHandler>
+              <PopoverContent className="absolute">
+              <div className='p-5'>
+              <div>
+              <img
+            src={thisBlog && thisBlog.data.author.authorPhoto}
+            alt="author"
+            className="w-10 h-10 rounded-full"
+          />
+          </div>
+          <br />
+          <h2 className="ml-4 text-cyan-600 font-medium text-center">{thisBlog && thisBlog.data.author.authorName}</h2>
+          <br />
+          <h2 className="ml-4 text-cyan-600 font-medium">Biography</h2>
+          <p>biography will be here</p>
+          <br />
+          <h2 className="ml-4 text-cyan-600 font-medium">Location</h2>
+          <p>location here</p>
+          </div>
+              </PopoverContent>
+            </Popover>
+            
+            </div>
       </div>
     </div>
   );
