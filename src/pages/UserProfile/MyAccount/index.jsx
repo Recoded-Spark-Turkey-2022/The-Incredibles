425d5fd2ee78077a7559a6868ca361c93d84cca8
batoul <batoul.tenbakji@gmail.com>
2023-01-25 19:36:04 +0000

    merge from main Fixes #134

--- src/pages/UserProfile/MyAccount/index.jsx
+++ src/pages/UserProfile/MyAccount/index.jsx
@@ -57,10 +57,18 @@ function MyAccount() {
             <img
               className="m-auto h-40 w-40 rounded-full"
               onClick={() => navigate('/myaccount/myaccountdetails')}
-              src={user.photoURL ? user.photoURL : user.authPhoto ? user.authPhoto:User}
+              src={
+                user.photoURL
+                  ? user.photoURL
+                  : user.authPhoto
+                  ? user.authPhoto
+                  : User
+              }
             />
             <h2 className="p-5 font-bold text-lg">
-              {user.username ? user.username + user.usersurname :user.displayName}
+              {user.username
+                ? user.username + user.usersurname
+                : user.displayName}
             </h2>
           </div>
           <div>
