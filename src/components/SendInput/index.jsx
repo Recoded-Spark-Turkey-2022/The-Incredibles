97ab30e1a79ae06db6795226b7e4cea603370210
Ahmad Al Hariri <100470190+ahmadmhd357@users.noreply.github.com>
2023-01-27 19:33:15 +0300

    remove the current version of picker-emoji and download the version 3.5.1 (npm i emoji-picker-react@3.5.1)

--- src/components/SendInput/index.jsx
+++ src/components/SendInput/index.jsx
@@ -5,6 +5,8 @@ import ChangePhoto from '../../assets/pics/profilepage/changepic.svg';
 import Images from '../../assets/pics/chatpage/images.svg';
 import Remove from '../../assets/pics/chatpage/delete.svg';
 import Send from '../../assets/pics/chatpage/send.svg';
+import Emoji from '../../assets/pics/chatpage/emoji.svg';
+
 import {
   arrayUnion,
   doc,
@@ -15,6 +17,7 @@ import {
 import { db, storage } from '../../firebase/firebase';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { v4 as uuid } from 'uuid';
+import Picker from 'emoji-picker-react';
 
 function SendInput() {
   const { chat } = useSelector((state) => state);
@@ -22,7 +25,7 @@ function SendInput() {
   const { chatUser, chatId } = chat;
   const [text, setText] = useState('');
   const [img, setImg] = useState(null);
-
+  const [openEmoji, setOpenEmoji] = useState(false);
   const handleSend = async () => {
     if (img) {
       const storageRef = ref(storage, uuid());
@@ -61,70 +64,90 @@ function SendInput() {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault(), handleSend;
-      }}
-      className="bg-white absolute w-full flex justify-between gap-8 bottom-0 h-20 max-sm:gap-0 p-4"
-    >
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        type="text"
-        placeholder="Type your message...."
-        className="w-full pl-4 bg-gray-200 placeholder:bg-gray-200 outline-none rounded-lg placeholder:p-4 max-sm:placeholder:p-2 max-sm:placeholder:text-sm max-sm:w-"
-      />
-
-      {img && (
-        <div className="relative">
-          {' '}
-          <img
-            className="w-14 h-14"
-            src={URL.createObjectURL(img)}
-            alt="img"
-          />{' '}
-          <img
-            onClick={() => setImg(null)}
-            className="w-4 h-4 absolute top-0 right-0 cursor-pointer"
-            src={Remove}
-            alt="delete"
-          />{' '}
-        </div>
-      )}
-      <div className="flex gap-4">
-        <input
-          onChange={(e) => setImg(e.target.files[0])}
-          type="file"
-          style={{ display: 'none' }}
-          id="file"
+    <>
+      <span className={openEmoji ? 'absolute bottom-20' : 'hidden'}>
+        <Picker
+          searchDisabled={true}
+          onEmojiClick={(event, emojiObject) => {
+            setText(text + emojiObject.emoji);
+          }}
         />
-        <label htmlFor="file">
-          <img
-            className="w-16 h-16 block pb-4 cursor-pointer max-sm:hidden hover:w-20 duration-500"
-            src={ChangePhoto}
-            alt="share"
-          />
-          <img
-            className="w-8 h-8 mt-2 hover:w-12 duration-500 cursor-pointer mx-2 hidden max-sm:flex"
-            src={Images}
-            alt="share"
-          />
-        </label>
-        <button
-          type="submit"
-          onClick={handleSend}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg max-sm:hidden"
-        >
-          send
-        </button>
+      </span>
+      <form
+        onSubmit={(e) => {
+          if (chatId) {
+            e.preventDefault(), handleSend();
+          } else {
+            e.preventDefault(), alert('really!! who are you sending to?!!');
+          }
+        }}
+        className="bg-white absolute w-full flex justify-between gap-8 bottom-0 h-20 max-sm:gap-0 p-4"
+      >
         <img
-          src={Send}
-          alt="send"
-          className="hidden hover:w-10 duration-500 cursor-pointer max-sm:flex w-6"
-          onClick={handleSend}
+          onClick={() => setOpenEmoji(!openEmoji)}
+          className="w-8 cursor-pointer"
+          src={Emoji}
+          alt=""
         />
-      </div>
-    </form>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          placeholder="Type your message...."
+          className="w-full pl-4 bg-gray-200 placeholder:bg-gray-200 outline-none rounded-lg placeholder:p-4 max-sm:placeholder:p-2 max-sm:placeholder:text-sm max-sm:w-"
+        />
+
+        {img && (
+          <div className="relative">
+            {' '}
+            <img
+              className="w-14 h-14"
+              src={URL.createObjectURL(img)}
+              alt="img"
+            />{' '}
+            <img
+              onClick={() => setImg(null)}
+              className="w-4 h-4 absolute top-0 right-0 cursor-pointer"
+              src={Remove}
+              alt="delete"
+            />{' '}
+          </div>
+        )}
+        <div className="flex gap-4">
+          <input
+            onChange={(e) => setImg(e.target.files[0])}
+            type="file"
+            style={{ display: 'none' }}
+            id="file"
+          />
+          <label htmlFor="file">
+            <img
+              className="w-16 h-16 block pb-4 cursor-pointer max-sm:hidden hover:w-20 duration-500"
+              src={ChangePhoto}
+              alt="share"
+            />
+            <img
+              className="w-8 h-8 mt-2 hover:w-12 duration-500 cursor-pointer mx-2 hidden max-sm:flex"
+              src={Images}
+              alt="share"
+            />
+          </label>
+          <button
+            type="submit"
+            onClick={(e)=>{if (chatId) {handleSend()} else{alert('really!! who are you sending to?!!')}}}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg max-sm:hidden"
+          >
+            send
+          </button>
+          <img
+            src={Send}
+            alt="send"
+            className="hidden hover:w-10 duration-500 cursor-pointer max-sm:flex w-6"
+            onClick={(e)=>{if (chatId) {handleSend()} else{alert('really!! who are you sending to?!!')}}}
+          />
+        </div>
+      </form>
+    </>
   );
 }
 
