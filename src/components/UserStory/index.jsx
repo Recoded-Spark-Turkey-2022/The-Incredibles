ffffb8b3025cbbe2e59ab07b0096562dea1f62c9
omer <3omar.tolip@gmail.com>
2023-01-29 16:19:06 +0300

    our sto ry section added also ar.json is ready with about page  info

--- src/components/UserStory/index.jsx
+++ src/components/UserStory/index.jsx
@@ -4,7 +4,10 @@ import story from '../../assets/pics/aboutpage/story.jpg';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase/firebase';
 import Container from '../UI/Container';
+import { useTranslation } from 'react-i18next';
+
 function CallToAction() {
+  const [t] = useTranslation();
   const [user] = useAuthState(auth);
   return (
     <Container>
@@ -13,19 +16,11 @@ function CallToAction() {
       </div>
       <div className="text-center p-10 max-md:px-0 max-md:py-5">
         <h4 className="font-normal text-3xl py-5 text-cyan-600">
-          Share your story, help others and get help from others
+        {t('about.story.h4')}
         </h4>
         <div className="text-center  ">
           <p className="text-zinc-400	font-light  sm:text-xl text-base ">
-            After years of struggling as a refugee, while I was seeking on
-            internet I found Refubook website and it changed my life immediately
-            finally found a job that allowed me to provide for my family. I
-            worked hard and saved every penny, determined to build a better
-            future for my children. With the help of a kind Refubook community,
-            I was able to put my kids through school and give them opportunities
-            I never had. As I watch them pursue their dreams, I am filled with
-            pride and gratitude for the journey that brought us here with this
-            amazing Refubook members.
+          {t('about.story.p')}
           </p>
         </div>
       </div>
