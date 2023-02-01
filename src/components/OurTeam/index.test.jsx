97f0b5f39622c2ac97a2c2c30632001dda067682
Rabia Kahya <rabiakahya22@gmail.com>
2023-02-01 12:47:39 +0300

    team photos added

--- src/components/OurTeam/index.test.jsx
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer';
-import OurTeam from '.';
-
-describe('OurTeam', () =>
-  it('renders correctly', () => {
-    const domTree = renderer.create(<OurTeam />).toJSON();
-    expect(domTree).toMatchSnapshot();
-  }));
